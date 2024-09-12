c094c537509da5fadcd92bc3f919fa5895853093
SebastianStankiewicz <sebstankiewicz23@gmail.com>
2024-09-12 11:39:49 +0100

    Changed contact me page

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,73 +1,117 @@
 import React from "react";
 
 export default function ContactForm() {
-    const [result, setResult] = React.useState("");
-  
-    const onSubmit = async (event) => {
-      event.preventDefault();
-      setResult("Sending....");
-      const formData = new FormData(event.target);
-  
-      formData.append("access_key", "dcfa130c-5f7d-4f8f-93b0-dfb15a600d2d"); 
-  
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        body: formData
-      });
-  
-      const data = await response.json();
-  
-      if (data.success) {
-        setResult("Form Submitted Successfully");
-        event.target.reset();
-      } else {
-        console.log("Error", data);
-        setResult(data.message);
-      }
-    };
+  const [result, setResult] = React.useState("");
 
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
 
-  
-    return (
-        <form onSubmit={onSubmit} className="max-w-md mx-auto space-y-4 p-6 bg-base-200 rounded-lg shadow-md">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Name</span>
-            </label>
-            <input type="text" name="fullName" placeholder="Enter your full name" className="input input-bordered" required />
-          </div>
-      
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input type="email" name="email" placeholder="Enter your email" className="input input-bordered" required />
-          </div>
-      
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Phone Number</span>
-            </label>
-            <input type="tel" name="phoneNumber" placeholder="Enter your phone number" className="input input-bordered" required />
-          </div>
-      
-          <input type="hidden" name="access_key" value="1909e76c-b5c4-48d0-a071-30f3adbec52f" />
-      
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Message</span>
-            </label>
-            <textarea name="message" placeholder="Your message" className="textarea textarea-bordered" required></textarea>
-          </div>
-      
-          <div className="form-control">
-            <button type="submit" className="btn btn-primary w-full">Submit Form</button>
-          </div>
-        </form>
-      );
-      
-      
-  }
+    formData.append("access_key", "dcfa130c-5f7d-4f8f-93b0-dfb15a600d2d");
 
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
 
-  /*           <input type="hidden" name="access_key" value="1909e76c-b5c4-48d0-a071-30f3adbec52f" /> */
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="max-w-md mx-auto space-y-4 p-6 bg-info rounded-lg shadow-md"
+    >
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Name</span>
+        </label>
+        <input
+          type="text"
+          name="fullName"
+          placeholder="Enter your full name"
+          className="input input-bordered"
+          required
+        />
+      </div>
+
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Company Name</span>
+        </label>
+        <input
+          type="text"
+          name="companyName"
+          placeholder="Enter your company name"
+          className="input input-bordered"
+          required
+        />
+      </div>
+
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Email</span>
+        </label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter your email"
+          className="input input-bordered"
+          required
+        />
+      </div>
+
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Phone Number</span>
+        </label>
+        <input
+          type="tel"
+          name="phoneNumber"
+          placeholder="Enter your phone number"
+          className="input input-bordered"
+          required
+        />
+      </div>
+
+      <input
+        type="hidden"
+        name="access_key"
+        value="dcfa130c-5f7d-4f8f-93b0-dfb15a600d2d"
+      />
+
+      <div className="form-control">
+        <label className="label">
+          <span className="label-text">Message</span>
+        </label>
+        <textarea
+          name="message"
+          placeholder="Your message"
+          className="textarea textarea-bordered"
+          required
+        ></textarea>
+      </div>
+
+      <div className="form-control">
+        <button type="submit" className="btn btn-primary w-full">
+          Submit Form
+        </button>
+      </div>
+    </form>
+  );
+}
+
+/*           <input type="hidden" name="access_key" value="1909e76c-b5c4-48d0-a071-30f3adbec52f" /> */
+
+//Harriet key - dcfa130c-5f7d-4f8f-93b0-dfb15a600d2d
+
+//Mine - 1909e76c-b5c4-48d0-a071-30f3adbec52f
